ae426fac6b6f77a2370ce9e6eda47129d7c31cd2
applestven <applestven@gmail.com>
2026-03-25 14:29:08 +0800

    ``` fix(api): 更新内部服务API地址配置
    - 将内部转录服务地址从 http://43.139.236.50:8686/tv
      更新为 http://139.199.192.179:8686/tv
    - 将内部下载服务地址从 http://43.139.236.50:8686/dv
      更新为 http://139.199.192.179:8686/dv
    - 同时更新了文档中的API代理机制说明
    ```

--- src/app/api/tv/[...path]/route.ts
+++ src/app/api/tv/[...path]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-const INTERNAL_BASE = process.env.TV_INTERNAL || 'http://43.139.236.50:8686/tv';
+const INTERNAL_BASE = process.env.TV_INTERNAL || 'http://139.199.192.179:8686/tv';
 
 async function proxy(req: Request | NextRequest, params?: Promise<{ path?: string[] }> | { path?: string[] }) {
     try {
